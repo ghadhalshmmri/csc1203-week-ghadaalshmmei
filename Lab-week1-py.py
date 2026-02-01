90f932d3c8d4018da4588caa85815b9c2c616078
ghadhalshmmri <ghadhalshmry58@gmail.com>
2026-02-01 23:15:59 +0300

    Lab-week1.py.ghadaALshmmri

---
+++ Lab-week1-py.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("welcom to python programming!")
+
+
+# In[2]:
+
+
+print("Name: Ghada Alshmmri ")
+print("Course: Advanced Pragramming Methods")
+
+
+# In[16]:
+
+
+student_name = "Alice"
+age = 20
+gpa = 3.75
+supjects = {"Math", "python", "physics"}
+studen_info ={"id": 101, "year":2 }
+skills = {"Python", "SQL"}
+is_enrolled = True
+graduation_data = None
+
+
+# In[17]:
+
+
+print(student_name, type(student_name))
+print(age, type(age))
+print(gpa, type(gpa))
+print(supjects, type(supjects))
+print(student_info, type(student_info))
+print(skills, type(skills))
+print(is_enrolled, type(is_enrolled))
+print(graduation_data, type(graduation_data))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
