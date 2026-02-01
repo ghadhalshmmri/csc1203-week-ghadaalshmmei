0bd44ae4328293ab1b9d48fdec13170aa7894271
ghadhalshmmri <ghadhalshmry58@gmail.com>
2026-02-01 22:58:36 +0300

    lab_week1.py

---
+++ lab_week1.py.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("welcom to python programming!")
+
+
+# In[2]:
+
+
+print("Name: Ghada Alshmmri ")
+print("Course: Advanced Pragramming Methods")
+
+
+# In[11]:
+
+
+print(student_name, type(student_name))
+print(age, type(age))
+print(gpa, type(gpa))
+print(supjects, type(supjects))
+print(student_info, type(student_info))
+print(skills, type(skills))
+print(is_enrolled, type(is_enrolled))
+print(graduation_data, type(graduation_data))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
